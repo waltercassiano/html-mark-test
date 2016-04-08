88ca604f08e55ffe6c1fc0302de9750f86785cad
W@lterCS <waltercassiano@gmail.com>
2016-04-08 15:39:02 -0300

    Start Project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# html-mark-test
+# HTML Register Mark Test
+
+# Usage
+
+mocha index.js --url="http://example.com" --wrapper=".selector"
+
+# Parameters
+
+Required
+--url
+
+Optional
+--wrapper
---
+++ index.js
@@ -0,0 +1,68 @@
+"use strict;"
+var request = require('request');
+var assert = require('chai').assert;
+var cheerio = require('cheerio');
+var $;
+var _body;
+var url;
+var selector_wrapper = 'body';
+var htmlentities = require('html-entities').AllHtmlEntities;
+
+if (!process.argv.slice(3)[0]) {
+  console.log('Plesase insert a URL');
+  process.exit(0);
+}
+
+url = process.argv.slice(3)[0].replace('--url=','');
+
+if (process.argv.slice(4)[0]) {
+  console.log('Wrapper Class HTML ' + process.argv.slice(4)[0]);
+  selector_wrapper = process.argv.slice(4)[0].replace('--wrapper=','');
+}
+
+/* default */
+
+var test = {
+  init: function() {
+    var scope = this;
+    describe('For a page requested ' + url, function(){
+      this.timeout(15000);
+      it('Should be a valid page', function(done){
+        request(url , function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            _body = body;
+            done();
+            //Call Test To the Page
+            $ = cheerio.load(_body);
+            scope.registerMark();
+            scope.tradeMark();
+          }
+          assert.isDefined(response, 'Can not get URL');
+        });
+      });
+    });
+  },
+  registerMark: function() {
+    var html_code = '&trade;';
+    var html_decoded = htmlentities.decode(html_code);
+    describe('For a Register Marks', function() {
+      it('Should it be inside sup tag', function(done) {
+        assert.strictEqual($(selector_wrapper + ' sup').text().match(/®/g).length, $(selector_wrapper).text().match(/®/g).length , '');
+        done();
+      });
+    });
+  },
+  tradeMark: function() {
+    var html_code = '&trade;';
+    var html_decoded = htmlentities.decode(html_code);
+    var regexexp = new RegExp(html_decoded,'g');
+    describe('For a TradeMarks code ' + html_code + ' -> ' + html_decoded, function() {
+      it('Should not be inside a sup tag', function(done) {
+        assert.isNotOk($(selector_wrapper + 'sup').text().match(regexexp) ? $(selector_wrapper + 'sup').text().match(regexexp).length : $(selector_wrapper + 'sup').text().match(regexexp),'There are html_decoded inside sup tag');
+        done();
+      });
+    });
+  }
+}
+
+test.init();
